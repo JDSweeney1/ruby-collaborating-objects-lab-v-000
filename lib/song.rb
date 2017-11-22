13e76fc778c666209f2f34e8cffdbe4b8c34f650
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-22 00:08:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    data = filename.split(" - ")
-    artist = data[0]
-    song_name = data[1]
-    genre = data[2].chop(".mp3")
+    artist, song = filename.split(" - ")
     new_song = self.new(song_name)
     new_song.artist_name = artist
     new_song
